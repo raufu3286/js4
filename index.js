30b10da356ea57c143c4ae72f0755a6ad1e1d912
eshikhon-mern-stack <mern2.eshikhon@gmail.com>
2023-06-25 01:48:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,286 @@
+//funciton
+
+//function add() {
+//    var a=10;
+//    var b=20;
+//    var c=20;
+ //   sum=(a+b+c);
+//    console.log(sum);
+//}
+//console.log("the sum:" );
+//add();
+
+// function number()
+//{
+ //   var n1=5;
+//    if (n1%2==0){
+//        console.log("The number is even");
+//    }
+//    else{
+//        console.log("the number is odd");
+//    }
+//}
+//number();
+
+// var a=50;
+//var b=60;
+// function add(){
+//    sum=(a+b);
+  //  console.log("the sum=" , sum);
+//}
+//function subtraction(){
+//    subtraction=(a-b);
+//    console.log("The subtraction= ",subtraction);
+//}
+// function multi(){
+//    multi=(a*b);
+//    console.log("The multiplication= ",multi);
+//}
+//function division(){
+//    division=(b/a);
+ //   console.log("The division= ",division);
+//}
+
+//add();
+//subtraction();
+//multi();
+//division();
+
+
+
+
+
+
+//function avg1(p,q){
+ //   sum=p+q;
+//    avg=sum/2;
+//    console.log("The average=",avg);
+    
+//}
+//avg1(100,200);
+
+//prime number or not
+//function prime(n){
+ //   var c=0;
+//    for(let i=2;i<=n;i++){
+//        if(n%i==0)
+//        {
+//            c=1;
+//            break;
+//        }
+//    }
+//    if(c==0)
+//    {
+//        console.log("The number is prime");
+//    }
+//    else{
+//        console.log("The number is not prime");
+//    }
+//   
+//
+//}
+//prime(17);
+
+//function numbers(n1,n2,n3){
+//   var sum=(n1+n2+n3);
+//   return sum;
+//}
+//var total=numbers(10,20,30);
+//console.log("total number= ",total)
+//avg=total/3;
+//console.log("The avg= ",avg);
+
+//funtion object
+//var student2={
+ //   id:2210,
+ //   department:"eng",
+ //   CGPA:2.56,
+ //   stautus:"Good",
+//
+//}
+//var student3={
+//var student2={
+//    id:2210,
+//    department:"eng",
+//    CGPA:2.56,
+//    stautus:"Good",
+
+//}
+//var student3={
+//    id:2210,
+//    department:"eng",
+//    CGPA:2.56,
+//    stautus:"Good",
+
+//}
+//var student4={
+//    id:2210,
+//    department:"eng",
+//    CGPA:2.56,
+//    stautus:"Good",
+//
+//}
+//console.log(student1);
+//console.log(student2);
+//console.log(student3);
+//console.log(student4);
+
+
+//switch
+//var ch="p";
+//switch(ch)
+//    {
+//        case 'a': console.log("vowel");
+//            break;
+//        case 'e':  console.log("vowel");
+//            break;
+//        case 'i':  console.log("vowel");
+//            break;
+//        case 'o':  console.log("vowel");
+//            break;
+//        case 'u':  console.log("vowel");
+//            break;
+//        case 'A': console.log("vowel");
+//            break;
+//        case 'E':  console.log("vowel");
+//            break;
+//        case 'I':  console.log("vowel");
+//            break;
+//        case 'O': console.log("vowel");
+//            break;
+//        case 'U': console.log("vowel");
+//            break;
+//        default:console.log("consonant");
+//    }
+
+var num1=101;
+var num2=105;
+switch(num1 > num2)
+    {
+        case false: console.log("Maximum number", num2);
+            break;
+
+        case true: console.log("Maximum number", num1);
+            break;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
